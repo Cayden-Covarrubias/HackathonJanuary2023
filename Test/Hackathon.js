c1ca8c2a850336cb10c11ca00c56c13c91b603a0
ShanMehr <ishanmeher25@gmail.com>
2023-01-21 22:35:34 -0800

    Restructured Project

--- Test/Hackathon.js
+++
@@ -1,8 +0,0 @@
-public class myclass{
-    public static void main(String[] args){
-        System.out.println("Hello World");
-        System.out.println("allo");
-        System.out.println("Hello World 2")
-        //Hello world
-    }
-}
